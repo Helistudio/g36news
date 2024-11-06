d0649cfb0d069b882e70833dd5ed156404a7fbbb
DESKTOP-TC2CSV1\K5Tiger <turbo98198@gmail.com>
2024-11-06 14:34:20 +0700

    add muti lang item

--- resources/views/gacha_history.blade.php
+++ resources/views/gacha_history.blade.php
@@ -1,49 +1,48 @@
 @extends("layouts.home")
-@section("body_class")gacha-history-container @endsection
+@section("body_class")
+    gacha-history-container
+@endsection
 @section("content")
     <div class="row gacha-history-section">
 
         <div class="col-12">
-            <?php if(count($gacha_history) > 0):?>
+            <?php if (count($gacha_history) > 0): ?>
             <table>
                     <?php
-                    $bonusItemType = "";
-                    $bonusItemName = "";
-                foreach($gacha_history as $index =>  $gh):
-                    $bonus = str_replace("[", "", $gh->bonus);
-                    $bonus = str_replace("]", "", $bonus);
-                    $arrayBonus = explode(",", $bonus);
-                    if(is_array($arrayBonus)) {
-                        if(isset($arrayBonus[0]) && $arrayBonus[0] == 100) {
+                        $bonusItemType = "";
+                        $bonusItemName = "";
+                        foreach ($gacha_history as $index => $gh):
+                            $bonus = str_replace("[", "", $gh->bonus);
+                            $bonus = str_replace("]", "", $bonus);
+                            $arrayBonus = explode(",", $bonus);
+                            if (is_array($arrayBonus)) {
+                                if (isset($arrayBonus[0]) && $arrayBonus[0] == 100) {
 
-                            $bonusItemType = "126";
-                            $bonusItemName = $heros[$arrayBonus[1]] ?? '';
-                        }
-                        else if(isset($arrayBonus[0]) && $arrayBonus[0] == 6) {
+                                    $bonusItemType = "126";
+                                    $bonusItemName = $heros[$arrayBonus[1]] ?? '';
+                                } else if (isset($arrayBonus[0]) && $arrayBonus[0] == 6) {
 
-                            $bonusItemType = "127";
-                            $bonusItemName = $heros[$arrayBonus[1]] ?? '';
-                        }
-                        else if(isset($arrayBonus[0]) && $arrayBonus[0] == 1) {
+                                    $bonusItemType = "127";
+                                    $bonusItemName = $heros[$arrayBonus[1]] ?? '';
+                                } else if (isset($arrayBonus[0]) && $arrayBonus[0] == 1) {
 
-                            $bonusItemType = "128";
-                            $bonusItemName = $itemShop[$arrayBonus[1]] ?? '';
-                        }
-                        else if(isset($arrayBonus[0]) && $arrayBonus[0] == 3) {
+                                    $bonusItemType = "128";
+                                    $bonusItemName =  {{ __('messages.itemLang'.$arrayBonus[1]) }} ?? '';
+                                } else if (isset($arrayBonus[0]) && $arrayBonus[0] == 3) {
 
-                            $bonusItemType = "128";
-                            $bonusItemName = $itemData[$arrayBonus[1]] ?? '';
+                                    $bonusItemType = "128";
+                                    $bonusItemName = $itemData[$arrayBonus[1]] ?? '';
+                                }
                         }
-                    }
                     ?>
                 <tr>
                     <td><span class="username">{{ $gh->screen_name }}</span> {{ __('messages.Lang129') }} {{ __('messages.Lang'.$bonusItemType) }} <span class="bonus"> {{ $bonusItemName }}</span></td>
                 </tr>
-                <?php endforeach;?>
+                <?php endforeach; ?>
             </table>
-            <?php else:?>
+            <?php else: ?>
             <div>{{ __('messages.Lang125') }}</div>
-            <?php endif;?>
+            <?php endif; ?>
         </div>
 
     </div>
