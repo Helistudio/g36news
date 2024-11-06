b949255675b2137f4e29731f33a258e3c7af3882
DESKTOP-TC2CSV1\K5Tiger <turbo98198@gmail.com>
2024-11-06 16:15:47 +0700

    no message

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'NewsController@getNews')->name('h39.news');
 Route::get('/lich-su-trieu-hoi', 'NewsController@getGachaHistory')->name('h39.gachaHistory');
+Route::get('/chi-tiet-trieu-hoi', 'NewsController@getGachaDetail')->name('h39.gachaDetail');
 Route::get('/chi-tiet-trieu-hoi-2', 'NewsController@getGachaDetail')->name('h39.gachaDetail');
 Route::get('/chi-tiet-trieu-hoi-4', 'NewsController@getGachaDetailFre')->name('h39.gachaDetailFre');
 Route::get('/chi-tiet-trieu-hoi-gioi-han', 'NewsController@getLimitedGachaDetail')->name('h39.limitedGachaDetail');
